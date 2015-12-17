1fd6df4b25a08cbab2e335defbdf7ce8d0c8cda4
Bagas Udi Sahsangka <udibagas@gmail.com>
2015-12-17 14:35:08 +0700

    aksi sesuai level

--- resources/views/psb/_list.blade.php
+++ resources/views/psb/_list.blade.php
@@ -4,13 +4,26 @@
 			<th>Nama/NISN Calon Siswa</th>
 			<th>JK</th>
 			<th>Tingkat</th>
+
+			@if (Auth::user()->role == 'keuangan' || Auth::user()->role == 'admin')
 			<th data-orderable="false">Metode, Tgl, Jml Pembayaran</th>
 			<th data-orderable="false">Asal/Tujuan Pembayaran</th>
 			<th>Status Pembayaran</th>
+			@endif
+
+			@if (Auth::user()->role == 'data' || Auth::user()->role == 'admin')
 			<th>Status Data</th>
+			@endif
+
+			@if (Auth::user()->role == 'test-wawancara' || Auth::user()->role == 'admin')
 			<th>Status Test, Wawancara</th>
+			@endif
+
+			@if (Auth::user()->role == 'seleksi' || Auth::user()->role == 'admin')
 			<th>Diterima</th>
-			<th data-orderable="false">Aksi</th>
+			@endif
+
+			<th data-orderable="false" style="width:150px;">Aksi</th>
 		</tr>
 	</thead>
 
@@ -25,6 +38,8 @@
 			</td>
 			<td>{{ $s->calonSiswa->jenis_kelamin == 1 ? 'L' : 'P' }}</td>
 			<td>{{ App\Psb::TingkatList($s->tingkat) }}</td>
+
+			@if (Auth::user()->role == 'keuangan' || Auth::user()->role == 'admin')
 			<td>
 				{{ $s->metode_pembayaran }}<br />
 				<i>{{ $s->tanggal_pembayaran }}</i><br />
@@ -42,6 +57,9 @@
 				<span class="label label-default">Belum Diverifikasi</span>
         		@endif
 			</td>
+			@endif
+
+			@if (Auth::user()->role == 'data' || Auth::user()->role == 'admin')
 			<td>
 				@if ($s->status_verifikasi_data)
 				<span class="label label-success">Lengkap</span><br />
@@ -50,6 +68,9 @@
 				<span class="label label-default">Belum Diverifikasi</span>
         		@endif
 			</td>
+			@endif
+
+			@if (Auth::user()->role == 'test-wawancara' || Auth::user()->role == 'admin')
 			<td>
 				@if ($s->status_test)
 				<span class="label label-success">Sudah Test</span><br />
@@ -57,6 +78,9 @@
 				<span class="label label-default">Belum Test</span>
         		@endif
 			</td>
+			@endif
+
+			@if (Auth::user()->role == 'seleksi' || Auth::user()->role == 'admin')
 			<td>
 				@if ($s->status == 1)
 				<span class="label label-success">Ya</span><br />
@@ -66,35 +90,31 @@
 				<span class="label label-default">Belum Dikonfirmasi</span>
         		@endif
 			</td>
+			@endif
+
 			<td>
-				@if (Auth::user()->role != 'pendaftar')
-
 				{!! Form::open(['method' => 'DELETE', 'url' => 'psb/'.$s->id, 'class' => 'form-inline']) !!}
-					@if ($s->status_pembayaran == 0)
-	        		<a href="/psb/sudahBayar/{{ $s->id }}" class="btn btn-success bayar btn-sm">Sudah Bayar</a> 
+					<div class="btn-group">
+						@if ($s->status_pembayaran == 0 && (Auth::user()->role == 'keuangan' || Auth::user()->role == 'admin'))
+		        		<a href="/psb/sudahBayar/{{ $s->id }}" class="btn btn-success bayar btn-sm">Sudah Bayar</a> 
 
-        			@elseif ($s->status_verifikasi_data == 0)
-	        		<a href="/psb/dataOk/{{ $s->id }}" class="btn btn-success bayar btn-sm">Data OK</a> 
+	        			@elseif ($s->status_verifikasi_data == 0 && (Auth::user()->role == 'data' || Auth::user()->role == 'admin'))
+		        		<a href="/psb/dataOk/{{ $s->id }}" class="btn btn-success bayar btn-sm">Data OK</a> 
 
-        			@elseif ($s->status_test == 0)
-	        		<a href="/psb/testOk/{{ $s->id }}" class="btn btn-success bayar btn-sm">Test OK</a> 
+	        			@elseif ($s->status_test == 0 && (Auth::user()->role == 'test-wawancara' || Auth::user()->role == 'admin'))
+		        		<a href="/psb/testOk/{{ $s->id }}" class="btn btn-success bayar btn-sm">Test OK</a> 
 
-        			@elseif ($s->status_test == 1 && $s->status == 0)
-	        		<a href="/psb/diterima/{{ $s->id }}" class="btn btn-success bayar btn-sm">Terima</a> 
-	        		<a href="/psb/ditolak/{{ $s->id }}" class="btn btn-danger bayar btn-sm">Tolak</a>
-        			@endif
+	        			@elseif ($s->status_test == 1 && $s->status == 0 && (Auth::user()->role == 'seleksi' || Auth::user()->role == 'admin'))
+		        		<a href="/psb/diterima/{{ $s->id }}" class="btn btn-success bayar btn-sm">Terima</a> 
+		        		<a href="/psb/ditolak/{{ $s->id }}" class="btn btn-danger bayar btn-sm">Tolak</a>
+	        			@endif
 
-	        		<!-- <a href="/psb/{{ $s->id }}/edit" class="btn btn-success btn-xs">Edit</a> -->
-	        		{!! Form::submit('Hapus', ['class' => 'btn btn-danger btn-sm delete']) !!}
+	        			@if (Auth::user()->role == 'admin')
+		        		{!! Form::submit('Hapus', ['class' => 'btn btn-danger btn-sm delete']) !!}
+		        		@endif
+		        		
+	        		</div>
         		{!! Form::close() !!}
-
-        		@else
-
-        			@if ($s->status_pembayaran == 1 && $s->step == 2)
-	        		<a href="/psb/step2/{{ $s->id }}" class="btn btn-success btn-sm">Isi Formulir</a>
-        			@endif
-
-        		@endif
 			</td>
 		</tr>
 		@endif
