aed3af29e2d9d17c04d306adb116ed5a39c457e1
Bagas Udi Sahsangka <udibagas@gmail.com>
2015-12-27 10:56:57 +0700

    benerin seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,31 +16,31 @@ class DatabaseSeeder extends Seeder
 
         // $this->call(UserTableSeeder::class);
 
-        DB::table('users')->insert([
-            'name'      => 'Panitia PSB MIAS',
-            'email'     => 'panitiapsbmias@gmail.com',
-            'password'  => bcrypt('password'),
-            'role'      => 'admin'
-        ]);
+        // DB::table('users')->insert([
+        //     'name'      => 'Panitia PSB MIAS',
+        //     'email'     => 'panitiapsbmias@gmail.com',
+        //     'password'  => bcrypt('password'),
+        //     'role'      => 'admin'
+        // ]);
 
-        DB::table('ta')->insert(['periode' => '2016/2017', 'active' => 1]);
+        // DB::table('ta')->insert(['periode' => '2016/2017', 'active' => 1]);
 
         DB::table('rekening')->insert([
-            'nama'          => 'BNI Syariah',
+            'bank'          => 'BNI Syariah',
             'pemegang'      => 'SDIT ASSUNNAH',
             'nomor'         => '1436 2015 25',
             'keterangan'    => 'Untuk Jenjang SD'
         ]);
 
         DB::table('rekening')->insert([
-            'nama'          => 'BNI Syariah',
+            'bank'          => 'BNI Syariah',
             'pemegang'      => 'SDIT ASSUNNAH',
             'nomor'         => '1436 2015 36',
             'keterangan'    => 'Untuk Jenjang SMP'
         ]);
 
         DB::table('rekening')->insert([
-            'nama'          => 'BNI Syariah',
+            'bank'          => 'BNI Syariah',
             'pemegang'      => 'SDIT ASSUNNAH',
             'nomor'         => '1436 2015 47',
             'keterangan'    => 'Untuk Jenjang SMA dan TBA'
